eba5dc8f62bc66170523f51d05c7bb397d22668a
TaimurZafar <34146102+TaimurZafar@users.noreply.github.com>
2018-02-15 13:49:40 +0000

    Add files via upload

---
+++ Dendrogram_Code/phylogenetictree_code.R
@@ -0,0 +1,29 @@
+source("http://www.bioconductor.org/biocLite.R")
+biocLite("ggtree")
+
+library("ape")
+library("Biostrings")
+library("ggplot2")
+library("ggtree")
+
+ENVtree <- read.tree("ENV_treefile")
+POLtree <- read.tree("POL_treefile")
+GAGtree <- read.tree("GAG_treefile")
+ORF1tree <- read.tree("ORF1_treefile.txt")
+ORF2tree <- read.tree("ORF2_treefile.txt")
+ORF0tree <- read.tree("ORF0_treefile.txt")
+
+ENV <- ggtree(ENVtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+POL <- ggtree(POLtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+GAG <- ggtree(GAGtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+
+ORF1 <- ggtree(ORF1tree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+ORF2 <- ggtree(ORF2tree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+ORF0 <- ggtree(ORF0tree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
+
+ENV
+POL
+GAG
+ORF1
+ORF2
+ORF0
