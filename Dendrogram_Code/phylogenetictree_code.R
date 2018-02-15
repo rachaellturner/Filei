0393c96208df1d71e96e149ae57d7d1142d5f7fe
TaimurZafar <34146102+TaimurZafar@users.noreply.github.com>
2018-02-15 13:52:58 +0000

    Update phylogenetictree_code.R

--- Dendrogram_Code/phylogenetictree_code.R
+++ Dendrogram_Code/phylogenetictree_code.R
@@ -1,11 +1,13 @@
 source("http://www.bioconductor.org/biocLite.R")
 biocLite("ggtree")
 
+#loading packages.
 library("ape")
 library("Biostrings")
 library("ggplot2")
 library("ggtree")
 
+#opening Newick format files for each ORF.
 ENVtree <- read.tree("ENV_treefile")
 POLtree <- read.tree("POL_treefile")
 GAGtree <- read.tree("GAG_treefile")
@@ -13,6 +15,7 @@ ORF1tree <- read.tree("ORF1_treefile.txt")
 ORF2tree <- read.tree("ORF2_treefile.txt")
 ORF0tree <- read.tree("ORF0_treefile.txt")
 
+#plotting phylogenetic tree for each ORF.
 ENV <- ggtree(ENVtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
 POL <- ggtree(POLtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
 GAG <- ggtree(GAGtree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
@@ -21,6 +24,7 @@ ORF1 <- ggtree(ORF1tree) + geom_tiplab(size = 4, colour = "blue") + geom_treesca
 ORF2 <- ggtree(ORF2tree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
 ORF0 <- ggtree(ORF0tree) + geom_tiplab(size = 4, colour = "blue") + geom_treescale()
 
+#printing each plot.
 ENV
 POL
 GAG
